342db2d70fc42e8e0001e537cb40011dc3016726
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-14 16:55:29 +0330

    fix edit supply chain modal

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,2 +1,3 @@
 export { default as LoginForm } from './loginForm/LoginForm';
 export { default as AddSupplyChainForm } from './addSupplyChainForm/AddSupplyChainForm';
+export { default as EditSupplyChainForm } from './editSupplyChainForm/EditSupplyChainForm';
--- src/components/modals/editSupplyChainModal/EditSupplyChainModal.tsx
+++ src/components/modals/editSupplyChainModal/EditSupplyChainModal.tsx
@@ -1,6 +1,7 @@
 import { Button, Modal } from "antd";
 import { useState } from "react";
-import { EditIcon } from "../../icons";
+import { AddSupplyChainForm } from "../../forms";
+import { AddIcon, EditIcon } from "../../icons";
 
 const EditSupplyChainModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,16 +17,20 @@ const EditSupplyChainModal = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  
+
   return (
     <>
       <Button className="bg-neutral-100 border-0 px-2" onClick={showModal}>
         <EditIcon className="text-neutral-400 hover:text-purple" />
       </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal
+        title="Edit Chain"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        <AddSupplyChainForm />
       </Modal>
     </>
   )
