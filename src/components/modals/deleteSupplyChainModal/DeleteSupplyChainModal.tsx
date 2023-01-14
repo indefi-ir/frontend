65cbcc9f543ef98561350d4a22743f7a8113f5db
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-14 17:09:23 +0330

    add delete modal

--- src/components/modals/deleteSupplyChainModal/DeleteSupplyChainModal.tsx
+++ src/components/modals/deleteSupplyChainModal/DeleteSupplyChainModal.tsx
@@ -16,16 +16,14 @@ const DeleteSupplyChainModal = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  
+
   return (
     <>
       <Button className="bg-neutral-100 border-0 px-2" onClick={showModal}>
         <DeleteIcon className="text-neutral-400 hover:text-purple" />
       </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Modal title="Delete Chain" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        Are You Sure?
       </Modal>
     </>
   )
