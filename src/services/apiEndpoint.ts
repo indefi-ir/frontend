cc0c98ea97b9018865ddd4c4e9bddbd9e4f02da1
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-21 06:56:44 +0330

    complete api endpoints

--- src/services/apiEndpoint.ts
+++ src/services/apiEndpoint.ts
@@ -1,3 +1,15 @@
-export const getSupplyChains =  "/api/SupplyChains";
-export const getCompanies =  "/api/Companies";
-export const addCompany = ""
+export const getSupplyChains =  "/api/supplyChains";
+export const addSupplyChain = "/api/supplyChains/add";
+export const editSupplyChain = "/api/supplyChains/delete";
+
+export const getCompanies =  "/api/companies";
+export const addCompany = "/api/companies/add";
+export const editCompany = "/api/companies/edit";
+
+export const getFinanciers =  "/api/financiers";
+export const addFinancier = "/api/financiers/add";
+export const editFinancier = "/api/financiers/edit";
+
+export const getSellOrders =  "/api/orders/sellorders";
+export const getBuyOrders = "/api/orders/buyorders";
+export const updateOrderState = "/api/orders/updatestate";
