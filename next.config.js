7683e97bf9685080b044835e8111646649c6b363
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-24 10:05:55 +0330

    fix docker file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const nextTranslate = require('next-translate');
+// const nextTranslate = require('next-translate');
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //   enabled: process.env.ANALYZE === 'true',
 // });
 
-module.exports = nextTranslate({
+module.exports = {
   serverRuntimeConfig: {},
   publicRuntimeConfig: {
     // Will be available on both server and client
     baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
   },
-});
+};
