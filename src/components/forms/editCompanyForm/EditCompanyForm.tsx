71837228360eb4917caea71a256c5b9c2efb5a8c
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-29 13:45:51 +0330

    add notification after request in edit company form

--- src/components/forms/editCompanyForm/EditCompanyForm.tsx
+++ src/components/forms/editCompanyForm/EditCompanyForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, notification } from "antd";
 import React, { useState } from "react";
 import { mutate } from "swr";
 import { companiesUrl, editCompanyUrl } from "../../../services/apiEndpoint";
@@ -13,53 +13,61 @@ interface Props {
 
 const EditCompanyForm = ({ closeModal, companyInfo }: Props) => {
   const [form] = Form.useForm();
-  const { id: regulatorId }: any = React.useContext(userInfoContext);
-  const [error, setError] = useState<string>("");
+  const [api, contextHolder] = notification.useNotification();
   const { id, name: companyName } = companyInfo?.companyInfo;
 
   const onFinish = async (values: any) => {
     const FinalData = { ...values, id }
     const result = await post(editCompanyUrl, FinalData)
     await mutate(companiesUrl);
-    if (result.status === undefined) {
+
+    if (result === '') {
       form.resetFields();
       closeModal();
-      setError("");
+      api.open({
+        message: 'The company was successfully edited.',
+        duration: 3,
+        className: 'success'
+      });
     } else {
-      setError(result?.response?.data)
       form.resetFields();
+      api.open({
+        message: `${result?.response?.data?.title}`,
+        duration: 3,
+        className: 'error'
+      });
     }
   }
 
   return (
-    <div className="mt-6">
-      {error &&
-        <div className="text-red-500 mb-4">{error}</div>
-      }
-      <Form
-        form={form}
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        onFinish={onFinish}
-        layout="horizontal"
-        initialValues={{ name: companyName }}
-      >
-        <Form.Item label="Name" name="name" >
-          <Input className="ml-2" />
-        </Form.Item>
-        <div className="ml-4">
-          <SetPasswordModal companyId={id}/>
-        </div>
-        <div className="mt-10 flex w-full flex-row-reverse">
-          <Button htmlType="submit" className="ml-2 bg-blue text-white hover:bg-blue-dark hover:!text-white">
-            Save
-          </Button>
-          <Button htmlType="button" onClick={() => closeModal()}>
-            Cancel
-          </Button>
-        </div>
-      </Form>
-    </div>
+    <>
+      {contextHolder}
+      <div className="mt-6">
+        <Form
+          form={form}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          onFinish={onFinish}
+          layout="horizontal"
+          initialValues={{ name: companyName }}
+        >
+          <Form.Item label="Name" name="name" >
+            <Input className="ml-2" />
+          </Form.Item>
+          <div className="ml-4">
+            <SetPasswordModal companyId={id} />
+          </div>
+          <div className="mt-10 flex w-full flex-row-reverse">
+            <Button htmlType="submit" className="ml-2 bg-blue text-white hover:bg-blue-dark hover:!text-white">
+              Save
+            </Button>
+            <Button htmlType="button" onClick={() => closeModal()}>
+              Cancel
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
   )
 }
 export default EditCompanyForm;
