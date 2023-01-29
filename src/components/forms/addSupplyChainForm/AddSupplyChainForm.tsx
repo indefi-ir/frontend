4e5d309a95e1095df532e3190e4c33847c4fdf8f
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-01-29 13:39:44 +0330

    add notification after request in supply chain form

--- src/components/forms/addSupplyChainForm/AddSupplyChainForm.tsx
+++ src/components/forms/addSupplyChainForm/AddSupplyChainForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Select, SelectProps } from "antd";
+import { Button, Form, Input, notification, Select, SelectProps } from "antd";
 import React from "react";
 import { useState } from "react";
 import { mutate } from "swr";
@@ -16,8 +16,8 @@ const AddSupplyChainForm = ({ closeModal }: Props) => {
   const [form] = Form.useForm();
   const { id: regulatorId }: any = React.useContext(userInfoContext);
   const [isFiledTouched, setIsFiledTouched] = useState<boolean>();
-  const [error, setError] = useState<string>("");
-  const { data: companies } = useSWR(companiesUrl, fetcher)
+  const [api, contextHolder] = notification.useNotification();
+  const { data: companies } = useSWR(companiesUrl, fetcher);
 
   const options: SelectProps['options'] = [];
   companies?.data?.map((company: { id: any; name: any; }) => (
@@ -30,53 +30,62 @@ const AddSupplyChainForm = ({ closeModal }: Props) => {
     const FinalData = { ...values, regulatorId }
     const result = await post(addSupplyChainUrl, FinalData)
     await mutate(supplyChainsUrl);
-    if (result.status === undefined) {
+
+    if (result === '') {
       form.resetFields();
       closeModal();
-      setError("");
+      api.open({
+        message: 'The chain was successfully created.',
+        duration: 3,
+        className: 'success'
+      });
     } else {
-      // setError(result?.response?.data)
       form.resetFields();
+      api.open({
+        message: `${result?.response?.data?.title}`,
+        duration: 3,
+        className: 'error'
+      });
     }
   }
 
   return (
-    <div className="mt-10">
-      {error &&
-        <div className="text-red-500 mb-4">{error}</div>
-      }
-      <Form
-        form={form}
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        onFinish={onFinish}
-        onFieldsChange={() => setIsFiledTouched(true)}
-        layout="horizontal">
-        <Form.Item label="Name" name="name" required>
-          <Input className="ml-2" />
-        </Form.Item>
-        <Form.Item label="Description" name="description" required>
-          <TextArea rows={4} className="ml-2" />
-        </Form.Item>
-        <Form.Item label="Companies" name="companies" required>
-          <Select
-            mode="tags"
-            size='middle'
-            style={{ width: '100%' }}
-            options={options}
-            className="ml-2"
-          />
-        </Form.Item>
-        <div className="mt-10 flex w-full flex-row-reverse">
-          <Button htmlType="submit" className="ml-2 bg-blue text-white hover:bg-blue-dark hover:!text-white">
-            Add
-          </Button>
-          <Button htmlType="button" onClick={() => closeModal()}>
-            Cancel
-          </Button>
-        </div>
-      </Form>
-    </div>
+    <>
+      {contextHolder}
+      <div className="mt-10">
+        <Form
+          form={form}
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 14 }}
+          onFinish={onFinish}
+          onFieldsChange={() => setIsFiledTouched(true)}
+          layout="horizontal">
+          <Form.Item label="Name" name="name" required>
+            <Input className="ml-2" />
+          </Form.Item>
+          <Form.Item label="Companies" name="companies" required>
+            <Select
+              mode="tags"
+              size='middle'
+              style={{ width: '100%' }}
+              options={options}
+              className="ml-2"
+            />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <TextArea rows={4} className="ml-2" />
+          </Form.Item>
+          <div className="mt-10 flex w-full flex-row-reverse">
+            <Button htmlType="submit" className="ml-2 bg-blue text-white hover:bg-blue-dark hover:!text-white">
+              Add
+            </Button>
+            <Button htmlType="button" onClick={() => closeModal()}>
+              Cancel
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
   )
 }
 export default AddSupplyChainForm
