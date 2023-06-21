d631547f463121f37bd34610d4a8c72e27841331
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-06-21 08:20:46 +0330

    Fix credit box in dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,29 +1,50 @@
-import { Card, Col, Row, Space } from "antd";
+import { Card, Col, Row, Segmented, Space } from "antd";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [options, setOptions] = useState(['ماه جاری', 'سال جاری', 'همه زمان ها']);
   return (
     <>
       <div>
+        <div className="flex justify-end mb-4">
+          <Segmented options={options} />
+        </div>
         <span className="block mb-2"> اعتبارات</span>
         <Row gutter={16}>
           <Col span={6}>
-            <Card title="Card title" bordered={false}>
-              Card content
+            <Card className="flex flex-col" bordered={false}>
+              <div className="flex justify-end">
+                <img src="/images/total-credit.png" alt="" />
+              </div>
+              <span className="text-black-500 block mb-2">کل اعتبار</span>
+              <span className="text-2xl block mb-2">۱۰۰۰ میلیون ریال</span>
             </Card>
           </Col>
           <Col span={6}>
-            <Card title="Card title" bordered={false}>
-              Card content
+            <Card className="flex flex-col" bordered={false}>
+              <div className="flex justify-end">
+                <img src="/images/cleared.png" alt="" />
+              </div>
+              <span className="text-black-500 block mb-2">تسویه شده</span>
+              <span className="text-2xl block mb-2">۲۵۰ میلیون ریال</span>
             </Card>
           </Col>
           <Col span={6}>
-            <Card title="Card title" bordered={false}>
-              Card content
+            <Card className="flex flex-col" bordered={false}>
+              <div className="flex justify-end">
+                <img src="/images/outstanding-debt.png" alt="" />
+              </div>
+              <span className="text-black-500 block mb-2">بدهی های معوقه</span>
+              <span className="text-2xl block mb-2">۲۵۰ میلیون ریال </span>
             </Card>
           </Col>
           <Col span={6}>
-            <Card title="Card title" bordered={false}>
-              Card content
+            <Card className="flex flex-col" bordered={false}>
+              <div className="flex justify-end">
+                <img src="/images/awaiting-payment.png" alt="" />
+              </div>
+              <span className="text-black-500 block mb-2">در انتظار پرداخت</span>
+              <span className="text-2xl block mb-2">۵۰ میلیون ریال</span>
             </Card>
           </Col>
         </Row>
