8bc2240cab3a68cebc963887951c8284fd044134
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-06-29 16:39:37 +0330

    Connect api to supplychain

--- src/pages/supply-chains/new-chain/index.tsx
+++ src/pages/supply-chains/new-chain/index.tsx
@@ -1,46 +1,36 @@
 import React, { useState } from 'react';
 import { Button, Card, Form, Input, Modal, Select, SelectProps } from 'antd';
-
-
-const { TextArea } = Input;
-
-import { InboxOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { message, Upload } from 'antd';
 import useSWR from 'swr';
 import 'beautiful-react-diagrams/styles.css';
-import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
-import { DiagramSchema } from "beautiful-react-diagrams/@types/DiagramSchema";
 import { fetcher, post } from '../../../services/axios';
 import { addSupplyChainUrl, companiesUrl } from '../../../services/apiEndpoint';
 import { DrawChain } from '../../../features';
-
-
 
 const NewChain = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [companiesList, setCompaniesList] = useState([]);
   const [nodesList, setNodesList] = useState<any>([])
-  const [chainInfo, setChainInfo] = useState();
+  const [chainInfo, setChainInfo] = useState([]);
+
   const showModal = () => {
     setIsModalOpen(true);
   };
 
-  console.log("chainInfo",chainInfo)
- 
-  const setIsParentData =  (chain:any) => (
-    setChainInfo(chain)
-  )
+  const getChainDataaa = (data: any) => {
+    setChainInfo(data);
+  };
+  const cloneChainInfo = chainInfo?.length > 0 ? [...chainInfo] : []
+  cloneChainInfo.shift();
 
   const handleOk = () => {
-  companiesList?.map((company: { value: any; label: any; }) => (
-    setNodesList((current: any) => [...current, {
-      id: company.value,
-       content: company.label, 
-       coordinates: [0, 0], 
-       outputs: [{ id: company.value + '_in' }, { id: company.value + '_out' }] ,
-    }])
-  ))
+    companiesList?.map((company: { value: any; label: any; }) => (
+      setNodesList((current: any) => [...current, {
+        id: company.value,
+        content: company.label,
+        coordinates: [0, 0],
+        outputs: [{ id: company.value + '_in' }, { id: company.value + '_out' }],
+      }])
+    ))
     setIsModalOpen(false);
   };
 
@@ -48,10 +38,9 @@ const NewChain = () => {
     setIsModalOpen(false);
   };
 
-  const handleChange = (value, options)=> {
+  const handleChange = (value: any, options: any) => {
     setCompaniesList(options)
   }
-
 
   const { data: companies } = useSWR(companiesUrl, fetcher);
   const options: SelectProps['options'] = [];
@@ -63,10 +52,10 @@ const NewChain = () => {
   ));
 
   const onFinish = async (values: any) => {
-    const finalData = { ...values }
+    console.log("values", values)
+    const finalData = { ...values, head: chainInfo[0], subChains: cloneChainInfo }
     console.log("values", finalData)
     const result = await post(addSupplyChainUrl, finalData);
-    // await mutate(companiesUrl);
     if (result.success) {
 
     }
@@ -85,21 +74,27 @@ const NewChain = () => {
         initialValues={{ remember: false }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
+        autoComplete="off">
         <Form.Item
           label="نام زنجیره"
           name="name"
-          rules={[{ required: true, message: 'لطفا نام زنجیره را وارد نمایید.' }]}
-        >
+          rules={[{
+            required: true,
+            message: 'لطفا نام زنجیره را وارد نمایید.'
+          }]}>
           <Input />
         </Form.Item>
-        <Form.Item name="logo">
+        <Form.Item name="logo" label="اسناد مربوط به زنجیره">
           <Input />
         </Form.Item>
-        <Form.Item name="description">
-          <span className='block mb-2'>توضیحات</span>
-          <TextArea rows={4} maxLength={6} />
+        <Form.Item
+          name="description"
+          label="توضیحات"
+          rules={[{
+            required: true,
+            message: 'لطفا توضیحات زنجیره را وارد نمایید.'
+          }]}>
+          <Input.TextArea rows={4} maxLength={6} />
         </Form.Item>
         {/* <Form.Item>
           <span className='block mb-2'>اسناد مربوط به زنجیره</span>
@@ -120,20 +115,19 @@ const NewChain = () => {
         </Form.Item>
       </Form>
       <div>
-      <Button onClick={showModal}>Add Node</Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <Select
-          mode="multiple"
-          style={{width: "100%"}}
-          allowClear
-          options={options}
-          onChange={( value, options)=>handleChange( value, options)}
-          placeholder="لطفا محصولات مورد نظر را انتخاب کنید."
-        />
-      </Modal>
+        <Button onClick={showModal}>Add Node</Button>
+        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <Select
+            mode="multiple"
+            style={{ width: "100%" }}
+            allowClear
+            options={options}
+            onChange={(value, options) => handleChange(value, options)}
+            placeholder="لطفا محصولات مورد نظر را انتخاب کنید."
+          />
+        </Modal>
       </div>
-      <DrawChain data={nodesList} setIsParentData={setIsParentData} />
-      
+      <DrawChain nodesList={nodesList} getChainData={getChainDataaa} />
     </Card>
   )
 }
