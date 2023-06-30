92c9f37aa8e4c807dd30799305366ed09fec8ef0
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-07-01 02:07:12 +0330

    Fix bug

--- src/components/adminMenu/index.tsx
+++ src/components/adminMenu/index.tsx
@@ -75,7 +75,7 @@ const AdminMenu = ({ styles }: Props) => {
   return (
     <Menu mode="inline" style={styles} className="text-base px-2">
       {
-        userInfo?.role === "Funder"
+        userInfo === "Funder"
           ? (adminMenuItems.map((item) => (
             <Menu.Item key={item.key} icon={item.icon} className="!my-6 text-black-500">
               <Link href={item.path}>
