0c672e49b8fd859ecac5f97081c7bf5ed5751412
Fateme Norouzi <fcp.norouzi@gmail.com>
2023-07-01 01:43:49 +0330

    Remove extera code

--- src/pages/company/requests/new-request/index.tsx
+++ src/pages/company/requests/new-request/index.tsx
@@ -2,63 +2,24 @@ import { } from '@ant-design/icons';
 import {
   Button,
   Card,
-  Cascader,
-  Checkbox,
-  DatePicker,
   Form,
   Input,
-  InputNumber,
-  Radio,
   Select,
   SelectProps,
-  Switch,
-  TreeSelect,
-  // Upload,
 } from 'antd';
 
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import useSWR, { mutate } from 'swr';
-const { RangePicker } = DatePicker;
 const { TextArea } = Input;
-
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
-import type { UploadChangeParam } from 'antd/es/upload';
-import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface';
-import { addNewInvoiceUrl, companiesUrl, InvoicesMyCompanyUrl, productCategoriesMyCompanyUrl, productCategoriesUrl, registerCompanyUrl } from '../../../../services/apiEndpoint';
+import { addNewInvoiceUrl, companiesUrl, InvoicesMyCompanyUrl, productCategoriesMyCompanyUrl } from '../../../../services/apiEndpoint';
 import { fetcher, post } from '../../../../services/axios';
 import { useRouter } from 'next/router';
-import { UserInfoContext } from '../../../../components/providers';
-
-const options: SelectProps['options'] = [];
-const getBase64 = (img: RcFile, callback: (url: string) => void) => {
-  const reader = new FileReader();
-  console.log(reader)
-  reader.addEventListener('load', () => callback(reader.result as string));
-  reader.readAsDataURL(img);
-};
-
-const beforeUpload = (file: RcFile) => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJpgOrPng && isLt2M;
-};
 
 const NewRequest = () => {
-
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>();
   const router = useRouter();
   const { data: productCategories } = useSWR(productCategoriesMyCompanyUrl, fetcher);
-
-  
-
   const { data: companies } = useSWR(companiesUrl, fetcher);
   
   const optionsCategories: SelectProps['options'] = [];
